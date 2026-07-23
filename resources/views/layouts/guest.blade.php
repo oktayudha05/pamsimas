7d586c3073af9efe50c1d3699322bfcd11cf2f64
oktayudha05 <oktayudha05@gmail.com>
2026-07-23 10:38:27 +0700

    fix: change logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,6 +11,9 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
 
+        <!-- Icon -->
+        <link rel="icon" type="image/png" href="{{ asset('logoo.png') }}">
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
